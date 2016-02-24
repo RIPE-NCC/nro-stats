642fa08e75a45b88969c7dddd8e518e08c3640d6
Varesh Tapadia <vtapadia@ripe.net>
2016-02-24 14:07:09 +0100

    Adding Spring Actuator

--- src/main/java/net/nro/stats/Application.java
+++ src/main/java/net/nro/stats/Application.java
@@ -1,9 +1,9 @@
 package net.nro.stats;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@EnableAutoConfiguration
+@SpringBootApplication
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
