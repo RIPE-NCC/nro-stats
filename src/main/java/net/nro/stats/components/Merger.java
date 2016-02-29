1d2f37f3ce7567215dcc6552e4eb58789af6e1b0
Jasper Timmer <jtimmer@ripe.net>
2016-02-29 18:09:27 +0100

    using new ipv4 merger

--- src/main/java/net/nro/stats/components/Merger.java
+++ src/main/java/net/nro/stats/components/Merger.java
@@ -29,10 +29,12 @@
  */
 package net.nro.stats.components;
 
+import net.nro.stats.components.merger.IPv4Merger;
 import net.nro.stats.components.parser.*;
 import net.nro.stats.resources.ParsedRIRStats;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -44,34 +46,19 @@ import java.util.stream.Collectors;
 public class Merger {
     private Logger logger = LoggerFactory.getLogger(this.getClass());
 
+    @Autowired
+    IPv4Merger iPv4Merger;
+
     @Value("${nro.stats.extended.order}")
     private String[] rirs;
-
-    class PrioritizedRecord<I extends Record> {
-        private int prio;
-        private Record record;
-
-        private PrioritizedRecord(int prio, Record record) {
-            this.prio = prio;
-            this.record = record;
-        }
-
-        public int getPrio() {
-            return prio;
-        }
-
-        public Record getRecord() {
-            return record;
-        }
-    }
 
     public List<Line> merge(List<ParsedRIRStats> parsedRIRStats) {
 
 
 
-        List<PrioritizedRecord<IPv4Record>> mergedIPv4Lines = new ArrayList<>();
-        List<PrioritizedRecord<IPv6Record>> mergedIPv6Lines = new ArrayList<>();
-        List<PrioritizedRecord<ASNRecord>> mergedASNLines = new ArrayList<>();
+        List<IPv4Record> mergedIPv4Lines = new ArrayList<>();
+        List<IPv6Record> mergedIPv6Lines = new ArrayList<>();
+        List<ASNRecord> mergedASNLines = new ArrayList<>();
 
         int i = 0;
         for( String rir : rirs) {
@@ -87,40 +74,38 @@ public class Merger {
             final int prio = i;
             mergedIPv4Lines.addAll(stats.getLines().stream()
                     .filter(record -> record instanceof IPv4Record)
-                    .map(record -> new PrioritizedRecord<IPv4Record>(prio, (IPv4Record)record))
-                    .sorted((o1, o2) -> ((IPv4Record)o1.getRecord()).getComparator().compare(o1.getRecord().getRange(), o2.getRecord().getRange()))
+                    .map(record -> (IPv4Record)record)
                     .collect(Collectors.toList()));
             mergedIPv6Lines.addAll(stats.getLines().stream()
                     .filter(record -> record instanceof IPv6Record)
-                    .map(record -> new PrioritizedRecord<IPv6Record>(prio, (IPv6Record)record))
-                    .sorted((o1, o2) -> ((IPv6Record)o1.getRecord()).getComparator().compare(o1.getRecord().getRange(), o2.getRecord().getRange()))
+                    .map(record -> (IPv6Record)record)
                     .collect(Collectors.toList()));
             mergedASNLines.addAll(stats.getLines().stream()
                     .filter(record -> record instanceof ASNRecord)
-                    .map(record -> new PrioritizedRecord<ASNRecord>(prio, (ASNRecord)record))
-                    .sorted((o1, o2) -> ((ASNRecord)o1.getRecord()).getComparator().compare(o1.getRecord().getRange(), o2.getRecord().getRange()))
+                    .map(record -> (ASNRecord)record)
                     .collect(Collectors.toList()));
 
             i++;
         }
 
-        List<Line> result = handleConflicts(mergedIPv4Lines);
-        result.addAll(handleConflicts(mergedIPv6Lines));
-        result.addAll(handleConflicts(mergedASNLines));
+        List<Line> result  = new ArrayList<>();
+        result.addAll( iPv4Merger.merge(mergedIPv4Lines));
+//        result.addAll(handleConflicts(mergedIPv6Lines));
+//        result.addAll(handleConflicts(mergedASNLines));
         return result;
     }
 
-    private <T extends Record> List<Line> handleConflicts(List<PrioritizedRecord<T>> lines) {
+    private <T extends Record> List<Line> handleConflicts(List<T> lines) {
         List<Line> results = new ArrayList<>();
 
         for(int i = 0; i < lines.size(); i++) {
 
-            if (i == 0 || !lines.get(i).getRecord().getRange().overlaps(lines.get(i-1).getRecord().getRange())) {
+            if (i == 0 || !lines.get(i).getRange().overlaps(lines.get(i-1).getRange())) {
 
-                results.add(lines.get(i).getRecord());
+                results.add(lines.get(i));
             }
             else {
-                logger.info("Conflict: " + lines.get(i).getRecord().toString());
+                logger.info("Conflict: " + lines.get(i).toString());
             }
         }
         return results;
